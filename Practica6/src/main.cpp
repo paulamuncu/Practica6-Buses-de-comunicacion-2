20d2f76d448c67c7456db27a977488841327b59b
paulamuncu <47222228+paulamuncu@users.noreply.github.com>
2021-06-02 21:31:47 +0200

    Add files via upload

---
+++ Practica6/src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.print("Iniciando SD ...");
+    SPI.begin(18,19,23,5);
+
+    if (!SD.begin(5)) {
+        Serial.println("No se pudo inicializar");
+        return;
+    }
+    Serial.println("inicializacion exitosa");
+
+    if (SD.exists("/archivo.txt")){
+
+            Serial.println("archivo.txt exists. ");
+
+            myFile=SD.open("/archivo.txt");
+
+        if (myFile) {
+        Serial.println("archivo.txt:");
+        while (myFile.available()) {
+
+            Serial.write(myFile.read());
+        }
+        myFile.close(); //cerramos el archivo
+    } 
+    else {
+
+        Serial.println("Error al abrir el archivo");
+    }
+    }
+    else{
+            Serial.println("archivo.txt doesn't exist");
+
+            myFile = SD.open("/archivo.txt", FILE_WRITE);//abrimos  el archivo 
+
+            myFile.println("Hola mundo");
+
+            myFile.close();
+
+            myFile=SD.open("/archivo.txt");
+
+    if (myFile) {
+        Serial.println("archivo.txt:");
+        while (myFile.available()) {
+
+            Serial.write(myFile.read());
+        }
+        myFile.close(); //cerramos el archivo
+    }
+    else {
+
+        Serial.println("Error al abrir el archivo");
+    } 
+
+    }
+}
+
+void loop()
+{ }
